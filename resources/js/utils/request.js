f76b21ed7b1357435bd830a82eedb5ff3fc3578f
Ziyad Hamed <ziyadhamedmoustafa@gmail.com>
2025-11-22 00:27:01 +0200

    after add jwt

--- resources/js/utils/request.js
+++ resources/js/utils/request.js
@@ -4,7 +4,7 @@ import { getToken, setToken } from '@/utils/auth';
 
 // Create axios instance
 const service = axios.create({
-    baseURL: process.env.MIX_BASE_API,
+    baseURL: process.env.MIX_BASE_API||'/api', // url = base url + request url
     timeout: 120000,
 });
 
